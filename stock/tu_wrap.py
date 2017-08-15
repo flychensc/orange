1626850f85519104bef6b0095ed170568d36ce14
Shawn Chen <flychensc@163.com>
2017-08-15 22:20:33 +0800

    准备Level 1中

--- stock/tu_wrap.py
+++ stock/tu_wrap.py
@@ -79,3 +79,156 @@ def get_k_data(code=None,
     """
     return ts.get_k_data(code, start, end, ktype, autype, index, retry_count,
                          pause)
+
+
+@lru_cache()
+def get_report_data(year, quarter):
+    """
+        获取业绩报表数据
+    Parameters
+    --------
+    year:int 年度 e.g:2014
+    quarter:int 季度 :1、2、3、4，只能输入这4个季度
+       说明：由于是从网站获取的数据，需要一页页抓取，速度取决于您当前网络速度
+       
+    Return
+    --------
+    DataFrame
+        code,代码
+        name,名称
+        eps,每股收益
+        eps_yoy,每股收益同比(%)
+        bvps,每股净资产
+        roe,净资产收益率(%)
+        epcf,每股现金流量(元)
+        net_profits,净利润(万元)
+        profits_yoy,净利润同比(%)
+        distrib,分配方案
+        report_date,发布日期
+    """
+    return ts.get_report_data(year, quarter)
+
+
+@lru_cache()
+def get_profit_data(year, quarter):
+    """
+        获取盈利能力数据
+    Parameters
+    --------
+    year:int 年度 e.g:2014
+    quarter:int 季度 :1、2、3、4，只能输入这4个季度
+       说明：由于是从网站获取的数据，需要一页页抓取，速度取决于您当前网络速度
+       
+    Return
+    --------
+    DataFrame
+        code,代码
+        name,名称
+        roe,净资产收益率(%)
+        net_profit_ratio,净利率(%)
+        gross_profit_rate,毛利率(%)
+        net_profits,净利润(万元)
+        eps,每股收益
+        business_income,营业收入(百万元)
+        bips,每股主营业务收入(元)
+    """
+    return ts.get_profit_data(year, quarter)
+
+
+@lru_cache()
+def get_operation_data(year, quarter):
+    """
+        获取营运能力数据
+    Parameters
+    --------
+    year:int 年度 e.g:2014
+    quarter:int 季度 :1、2、3、4，只能输入这4个季度
+       说明：由于是从网站获取的数据，需要一页页抓取，速度取决于您当前网络速度
+       
+    Return
+    --------
+    DataFrame
+        code,代码
+        name,名称
+        arturnover,应收账款周转率(次)
+        arturndays,应收账款周转天数(天)
+        inventory_turnover,存货周转率(次)
+        inventory_days,存货周转天数(天)
+        currentasset_turnover,流动资产周转率(次)
+        currentasset_days,流动资产周转天数(天)
+    """
+    return ts.get_operation_data(year, quarter)
+
+
+@lru_cache()
+def get_growth_data(year, quarter):
+    """
+        获取成长能力数据
+    Parameters
+    --------
+    year:int 年度 e.g:2014
+    quarter:int 季度 :1、2、3、4，只能输入这4个季度
+       说明：由于是从网站获取的数据，需要一页页抓取，速度取决于您当前网络速度
+       
+    Return
+    --------
+    DataFrame
+        code,代码
+        name,名称
+        mbrg,主营业务收入增长率(%)
+        nprg,净利润增长率(%)
+        nav,净资产增长率
+        targ,总资产增长率
+        epsg,每股收益增长率
+        seg,股东权益增长率
+    """
+    return ts.get_growth_data(year, quarter)
+
+
+@lru_cache()
+def get_debtpaying_data(year, quarter):
+    """
+        获取偿债能力数据
+    Parameters
+    --------
+    year:int 年度 e.g:2014
+    quarter:int 季度 :1、2、3、4，只能输入这4个季度
+       说明：由于是从网站获取的数据，需要一页页抓取，速度取决于您当前网络速度
+       
+    Return
+    --------
+    DataFrame
+        code,代码
+        name,名称
+        currentratio,流动比率
+        quickratio,速动比率
+        cashratio,现金比率
+        icratio,利息支付倍数
+        sheqratio,股东权益比率
+        adratio,股东权益增长率
+    """
+    return ts.get_debtpaying_data(year, quarter)
+
+
+@lru_cache()
+def get_cashflow_data(year, quarter):
+    """
+        获取现金流量数据
+    Parameters
+    --------
+    year:int 年度 e.g:2014
+    quarter:int 季度 :1、2、3、4，只能输入这4个季度
+       说明：由于是从网站获取的数据，需要一页页抓取，速度取决于您当前网络速度
+       
+    Return
+    --------
+    DataFrame
+        code,代码
+        name,名称
+        cf_sales,经营现金净流量对销售收入比率
+        rateofreturn,资产的经营现金流量回报率
+        cf_nm,经营现金净流量与净利润的比率
+        cf_liabilities,经营现金净流量对负债比率
+        cashflowratio,现金流量比率
+    """
+    return ts.get_cashflow_data(year, quarter)
