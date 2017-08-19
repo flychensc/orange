5001c75321766ee5b676a2768ccef6b90f0195ee
Shawn Chen <flychensc@163.com>
2017-08-19 20:18:28 +0800

    融资融券

--- stock/__init__.py
+++ stock/__init__.py
@@ -6,3 +6,4 @@ from stock.website import get_balance_sheet, get_profit_statement
 from stock.fundamental import get_annual_report, get_quarterly_results
 from stock.fundamental import get_basic_info, get_level0_report, get_level1_report
 from stock.fundamental import classifier_level_report, pct_change
+from stock.technical import get_margin_details
---
+++ stock/technical.py
@@ -0,0 +1,56 @@
+"""
+技术面
+"""
+
+import gevent
+
+import stock.tu_wrap as ts
+import pandas as pd
+
+from gevent.pool import Group
+
+MARGIN_COLUMNS = ['融资余额(元)', '融资买入额(元)', '融券余量', '融券卖出量']
+
+
+def _get_sz_margin_details(date, output_list):
+    output_list.append(ts.sz_margin_details(date=date))
+
+
+def get_margin_details(code, start, end):
+    """
+    获取融资融券明细列表
+    Parameters
+    --------
+    code：string
+                股票代码, e.g.600728
+    start:string
+                开始日期 format：YYYY-MM-DD 默认为空''
+    end:string
+                结束日期 format：YYYY-MM-DD 默认为空''
+    Return
+    ------
+    DataFrame
+    opDate:信用交易日期
+    rzye:本日融资余额(元)
+    rzmre: 本日融资买入额(元)
+    rqyl: 本日融券余量
+    rqmcl: 本日融券卖出量
+    """
+    sh_details = ts.sh_margin_details(start=start, end=end)
+
+    sz_list = list()
+    group = Group()
+    for date in sh_details['opDate'].drop_duplicates():
+        group.add(gevent.spawn(_get_sz_margin_details, date, sz_list))
+    group.join()
+    sz_details = pd.concat(sz_list)
+
+    details = pd.concat([
+        sh_details[['opDate', 'stockCode', 'rzye', 'rzmre', 'rqyl', 'rqmcl']],
+        sz_details[['opDate', 'stockCode', 'rzye', 'rzmre', 'rqyl', 'rqmcl']]
+    ])
+
+    detail = details.where(details['stockCode'] == code).dropna()
+    detail.drop(['stockCode'], axis=1).set_index('opDate')
+    detail.columns = MARGIN_COLUMNS
+    return detail
