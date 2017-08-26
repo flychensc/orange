d8a5c089c6f2c6e65a95d641dad64f844247d740
Shawn Chen <flychensc@163.com>
2017-08-26 20:49:18 +0800

    +webgui

--- navel/urls.py
+++ navel/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^web/', include('webgui.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^backend/', include('backend.urls')),
 ]
