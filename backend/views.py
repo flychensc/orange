5094957e5c82e0606989aff010a8999da3d2472d
Shawn Chen <flychensc@163.com>
2017-08-26 23:52:05 +0800

    差强人意的效果图

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
+import json
 from stock import get_tick_data
 
 # Create your views here.
@@ -9,4 +10,15 @@ def tick_data(request, code):
     start = request.GET.get('start')
     end = request.GET.get('end')
     tick_data = get_tick_data(code, start, end)
-    return HttpResponse("%(tick_data)s" % locals())
+
+    data_dict = dict()
+    data_dict['buy'] = [[
+        row[1]['日期'] + ' ' + row[1]['时间'],
+        row[1]['成交额'],
+    ] for row in tick_data[tick_data['买卖类型'] == '买盘'].iterrows()]
+    data_dict['sell'] = [[
+        row[1]['日期'] + ' ' + row[1]['时间'],
+        row[1]['成交额'],
+    ] for row in tick_data[tick_data['买卖类型'] == '卖盘'].iterrows()]
+
+    return JsonResponse(data_dict)
