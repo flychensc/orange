3f45f7d9e436308889efe057b9ca23717a924cd7
Shawn Chen <flychensc@163.com>
2017-08-26 21:09:34 +0800

    ECharts 入门

--- webgui/views.py
+++ webgui/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def tick_data(request, code):
+    start = request.GET.get('start')
+    end = request.GET.get('end')
+    return render(request, 'tick_data.html')
