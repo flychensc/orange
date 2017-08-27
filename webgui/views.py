36e8318f5f4ca5979aa24e669da3fe1c1a51bb56
Shawn Chen <flychensc@163.com>
2017-08-27 16:23:32 +0800

    微调布局

--- webgui/views.py
+++ webgui/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from stock import get_basic_info
+
 # Create your views here.
 
 def tick_data(request, code):
     start = request.GET.get('start')
     end = request.GET.get('end')
+    basic = get_basic_info(code)
+    name = basic['名称']
+    outstanding = basic['市值(亿)']
     return render(request, 'tick_data.html', locals())
