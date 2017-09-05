e0b1b964d687b650a344459cb904b92c60ac3752
Shawn Chen <flychensc@163.com>
2017-09-05 22:51:44 +0800

    股票列表

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<code>[0-9]+)/annual_report$', views.annual_report, name='tick_data'),
+    url(r'^(?P<code>[0-9]+)/annual_report$', views.annual_report, name='annual_report'),
+    url(r'^stock_list$', views.stock_list, name='stock_list'),
     url(r'^(?P<code>[0-9]+)/tick_data$', views.tick_data, name='tick_data'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 
 import json
 from stock import get_annual_report, get_tick_data, pct_change
+from stock.tu_wrap import get_stock_basics
 
 # Create your views here.
 
@@ -10,30 +11,39 @@ from stock import get_annual_report, get_tick_data, pct_change
 def annual_report(request, code):
     recent = request.GET.get('recent')
 
-    annual_report = get_annual_report(code)
+    report = get_annual_report(code)
     if recent:
-        annual_report = annual_report[annual_report.columns.tolist()[-int(recent):]]
+        report = report[report.columns.tolist()[-int(recent):]]
 
-    annual_report.rename(columns=lambda x: str(x)[:10], inplace=True)
+    report.rename(columns=lambda x: str(x)[:10], inplace=True)
 
-    year_yoy = pct_change(annual_report, axis=1)
+    year_yoy = pct_change(report, axis=1)
     year_yoy = (year_yoy * 100).round(2)
 
-    annual_report.fillna(0, inplace=True)
+    report.fillna(0, inplace=True)
     year_yoy.fillna(0, inplace=True)
 
     data_dict = dict()
-    data_dict['date'] = annual_report.columns.tolist()
+    data_dict['date'] = report.columns.tolist()
 
-    data_dict['income'] = annual_report.loc['销售额'].values.tolist()
-    data_dict['profit'] = annual_report.loc['净利润'].values.tolist()
-    data_dict['liability'] = annual_report.loc['所有债务'].values.tolist()
+    data_dict['income'] = report.loc['销售额'].values.tolist()
+    data_dict['profit'] = report.loc['净利润'].values.tolist()
+    data_dict['liability'] = report.loc['所有债务'].values.tolist()
 
     data_dict['income_yoy'] = year_yoy.loc['销售额'].values.tolist()
     data_dict['profit_yoy'] = year_yoy.loc['净利润'].values.tolist()
     data_dict['liability_yoy'] = year_yoy.loc['所有债务'].values.tolist()
 
     return JsonResponse(data_dict)
+
+
+def stock_list(request):
+    return JsonResponse({
+        "stocks": [
+            "%s %s" % (row[1].name, row[1]['name'])
+            for row in get_stock_basics().iterrows()
+        ]
+    })
 
 
 def tick_data(request, code):
