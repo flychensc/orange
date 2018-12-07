50dd1715371b729fc5bc5aad5e3f71268d0ec429
flychensc <flychensc@163.com>
2018-12-08 00:45:55 +0800

    导出API

--- stock/__init__.py
+++ stock/__init__.py
@@ -6,4 +6,4 @@ from stock.website import get_balance_sheet, get_profit_statement
 from stock.fundamental import get_annual_report, get_quarterly_results
 from stock.fundamental import get_basic_info, get_level0_report, get_level1_report
 from stock.fundamental import classifier_level_report, pct_change
-from stock.technical import get_margin_details, get_tick_data
+from stock.technical import get_sh_margin_details, get_sz_margin_details, get_tick_data
