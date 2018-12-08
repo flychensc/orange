a66eb8884a5eecbe4c8d48116b205c9d3b064905
flychensc <flychensc@163.com>
2018-12-09 01:27:39 +0800

    这里应该为空，否则django无法启动

--- storage/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-storage API
-"""
-
-from storage.stock import  (get_stock_basics, get_k_data,
-                    get_report_data, get_profit_data,
-                    get_operation_data, get_growth_data,
-                    get_debtpaying_data, get_cashflow_data)
