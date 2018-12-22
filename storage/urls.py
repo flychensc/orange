4e8095edc0d65e2ca3781c41d4b5d51701379234
flychensc <flychensc@163.com>
2018-12-22 23:13:55 +0800

    显示数据库的更新时间

--- storage/urls.py
+++ storage/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^update/history$', views.update_history, name='update_history'),
     url(r'^update/tick_data$', views.update_tick_data, name='update_tick_data'),
     url(r'^update/fundamental$', views.update_fundamental, name='update_fundamental'),
+    url(r'^info/update_time$', views.update_time, name='update_time'),
 ]
