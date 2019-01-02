bf795f6b484b93f335f7fc6daf9032bf0e0d7cc0
flychensc <flychensc@163.com>
2019-01-02 23:15:12 +0800

    网易接口，3分半 1. 支持当日数据 2. 不支持复权

--- stock/__init__.py
+++ stock/__init__.py
@@ -2,7 +2,7 @@
 stock API
 """
 
-from stock.website import get_balance_sheet, get_profit_statement
+from stock.website import get_balance_sheet, get_profit_statement, get_k_data
 from stock.fundamental import get_annual_report, get_quarterly_results
 from stock.fundamental import get_stock_basics, get_basic_info 
 from stock.fundamental import get_level0_report, get_level1_report
@@ -12,5 +12,5 @@ from stock.fundamental import (get_report_data, get_profit_data,
                                 get_debtpaying_data, get_cashflow_data)
 from stock.fundamental import get_bdi, get_shibor
 from stock.technical import get_sh_margin_details, get_sz_margin_details
-from stock.technical import get_tick_data, get_k_data, get_szzs
+from stock.technical import get_tick_data, get_szzs
 from stock.news import get_notices
